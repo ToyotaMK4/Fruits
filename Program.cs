0683de5ed808cdae8211c71fb94ee74d73acf4ea
AfruzCoder <afruzhusanov1@gmail.com>
2024-03-02 12:09:46 +0500

    homework

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOP.service;
+Person person = new Person();
+System.Console.WriteLine(person.Last_name);
+System.Console.WriteLine(person.First_name);
+System.Console.WriteLine(person.Age);
---
+++ servise/MevaService.cs
@@ -0,0 +1,31 @@
+namespace OOP.service
+{
+    public class Person
+    {
+        string first_name = "Kamron";
+        string last_name = "mustafoyev";
+        int age = 13;
+        public string First_name
+        {
+            get
+            {
+                return First_name;
+            }
+        }
+        public string Last_name
+        {
+            get
+            {
+                return Last_name;
+            }
+        }
+        public int Age
+        {
+           get
+           {
+                return Age;
+           } 
+        }
+        
+    }
+}
